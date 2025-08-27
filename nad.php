267d8571d8f0ca8886c0a17e0661520881a0be9f
nadhirah <ainnurnadhirah04@gmail.com>
2025-08-27 16:15:22 +0800

    add new file

---
+++ nad.php
@@ -0,0 +1,11 @@
+<?php
+// Simple PHP test script
+
+echo "Hello, PHP is working!<br>";
+
+// Test PHP version
+echo "PHP Version: " . phpversion() . "<br>";
+
+// Test date function
+echo "Current Date & Time: " . date("Y-m-d H:i:s");
+?>
